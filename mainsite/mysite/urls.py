16f0bbacd5bbd18131b244824495c6d9ddbbf17d
Breacher1337 <elaijahangelo@gmail.com>
2022-11-03 21:29:38 +0800

    added settings.py, url.py in mainsite

--- mainsite/mysite/urls.py
+++ mainsite/mysite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from . import views
+
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('admin/', admin.site.urls),
+    path('first_app/', include("first_app.urls")),
+    path("", views.home_view)
+
 ]
