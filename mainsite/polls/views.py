073a9428c679c4314c12c5c338ae015d8f18aac4
Breacher1337 <elaijahangelo@gmail.com>
2022-11-15 20:40:16 +0800

    update polls

--- mainsite/polls/views.py
+++ mainsite/polls/views.py
@@ -1,5 +1,5 @@
 
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, Http404
 
 from .models import Question
@@ -8,15 +8,10 @@ from .models import Question
 
 def index(request):
     latest_question_list = Question.objects.order_by("-pub_date")[:5]
-    context = {'latest_question_list': latest_question_list}
-    
-    return render(request, 'polls/index.html', context)
+    return render(request, 'polls/index.html', {'latest_question_list': latest_question_list})
 
 def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     return render(request, 'polls/detail.html', {'question': question})
 
 def results(request, question_id):
