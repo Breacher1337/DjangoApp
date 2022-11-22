d89ded53f49cfac34b6d13662c956e172b71c9ea
Breacher1337 <elaijahangelo@gmail.com>
2022-11-22 18:13:50 +0800

    Added 'testserver' on ALLOWED_HOSTS

--- mainsite/mysite/settings.py
+++ mainsite/mysite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = secrets["SETTINGS_SECRET_KEY"]
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["testserver"]
 
 # Application definition
 
