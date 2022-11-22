e14934834b4491535a625f1417c1a6fde23f659e
Breacher1337 <elaijahangelo@gmail.com>
2022-11-22 18:13:12 +0800

    Update models.py, tests.py, urls.py, views.py

--- mainsite/polls/views.py
+++ mainsite/polls/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question, Choice
 
@@ -13,7 +14,7 @@ class IndexView(generic.ListView):
     context_object_name = 'latest_question_list'
 
     def get_queryset(self):
-        return Question.objects.order_by('-pub_date')[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now().order_by('-pub_date'))[:5]
     
 class DetailView(generic.DetailView):
     model = Question
@@ -30,7 +31,7 @@ def vote(request, question_id):
     except (KeyError, Choice.DoesNotExist):
         return render(request, 'polls/detail.html', {
             "question": question,
-            "error_message": "You did't select a choice.",
+            "error_message": "You didn't select a choice.",
         })
     else:
         selected_choice.votes += 1
